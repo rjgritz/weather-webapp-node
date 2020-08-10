64eaf71fd79300c339369ef9ff78c624ed10a8de
Raquel <junqueirachel06@gmail.com>
2020-08-09 22:39:05 -0300

    Add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (lat, long, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${lat},${long}&units=m`
 
     request({ url, json: true }, (error, response) => {
-        const { weather_descriptions, temperature, feelslike } = response.body.current
+        const { weather_descriptions, temperature, feelslike, humidity } = response.body.current
 
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         }
         else {
-           callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degress out.')
+           callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degress out. The humidity is ' + humidity + '%.')
         }
     })
 }
